ef7ff6b180cba400dbf74484c2f87c002ea3e3ec
chienxu10x <chienxu10x@gmail.com>
2023-08-02 13:44:44 +0700

    drawer layout

--- app/src/main/java/com/example/appcc/activity/MainActivity.kt
+++ app/src/main/java/com/example/appcc/activity/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : BaseActivity() {
 //        appBarConfiguration.
     }
 
-
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_menu -> {
             // User chose the "Settings" item, show the app settings UI...
